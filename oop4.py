42e154d4066d80b2ab4a0191e2ceb3cccaca82fa
alif <alif@admin.com>
2020-08-09 13:04:26 +0600

    working of file 4

--- oop4.py
+++ oop4.py
@@ -31,6 +31,34 @@ class Developer(Employee):
         self.prog_lang = prog_lang
 
 
+class Manager(Employee):
+
+    def __init__(self , first , last , pay  , employees = None):
+
+        super().__init__(first , last , pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+
+
+    def add_emp(self , emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self , emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+        
+    def print_emps(self):
+
+        for emp in self.employees:
+            print('-->' , emp.fullName())
+
+
+
 
 
 
@@ -49,7 +77,18 @@ dev_2 = Developer('nj' , 'Developer', 28000 , 'java')
 
 # print(help(Developer))   eta diye  kon theke ki ki ashtese ta dekha jay 
 
-print(dev_1.pay)
-dev_1.apply_raise()
-print(dev_1.pay)
-print(dev_1.prog_lang)
+# print(dev_1.pay)
+# dev_1.apply_raise()
+# print(dev_1.pay)
+# print(dev_1.prog_lang)
+# print(dev_1.email)
+
+
+mrg_1 = Manager('sue' , ' smith' , 9000 , [dev_1])
+mrg_1.add_emp(dev_2)
+
+
+
+print(mrg_1.email)
+
+mrg_1.print_emps()
